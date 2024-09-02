0abd0b38e5003832690d020e0b19fc38017cbb96
jareds-laf <sofairj@lafayette.edu>
2024-09-02 01:51:54 -0400

    Making less of a mess
    Updated plot_timdep_norms.py to be a bit more flexible in its inputs. You can now specify (at the top of the file) a flag variable (time_averaged). When True, this flag will plot the time-averaged norms of your data (assuming you've computed these values already). When False, it will plot the time-dependent norms of your data (again, assuming you've calculated this already). I still need to get it so that this will process the data and compute the norms automatically. I will also have to update plot_spectra.py to be less messy.

--- plot_timedep_norms.py
+++ plot_timedep_norms.py
@@ -5,6 +5,8 @@ import numpy as np
 import glob
 from tkinter import filedialog
 
+time_averaged = False
+
 def normalize_path(in_path):
     # A quick function to ensure that any input paths are properly referenced
     return os.path.normpath(os.path.realpath(os.path.expanduser(in_path)))
@@ -15,14 +17,14 @@ def find_nearest(array, value):
     return array[idx]
 
 
-# Read in the spectrum analyzer data
-files = glob.glob(r'G:\My Drive\Other\REUs\Summer 2024\UCD\Data\007\norms_Tek000_*_ALL.csv')
+# Read in the norms
+files = glob.glob(r'G:\My Drive\Other\REUs\Summer 2024\UCD\Data\008\norms_Tek001_*_ALL.csv')
 data = []
 
 for i in files:
     if i:
         print(i)
-    data.append(pd.read_csv(i,skiprows=14,names=['TIME', 'CH1', 'CH2', 'CH3', 'CH4', 'Norm (V)', 'Norm (B)']))
+    data.append(pd.read_csv(i,sep='\t',skiprows=1,names=['TIME', 'CH1', 'CH2', 'CH3', 'CH4', 'Norm (V)', 'Norm (B)']))
 
 # print(data[0])
 
@@ -50,31 +52,35 @@ fig, ax = plt.subplots()
 
 bar_colors = ['#062bb4', '#2822d4', '#822ee8', '#d96ae0', '#ee94a7', '#fcc2cf']
 
+if time_averaged:
+    avgs = []
+    for i in data:
+        avgs.append(np.mean(i['Norm (B)'] / 1e-6))
+    
+    bar_labels = ['Breaker on', 'Breaker off']
+    # bar_labels = ['Motor', 'Background', 'Paint A', 'Paint B', 'Paint C', 'No Paint']
+    # ax.axhline(y=np.mean(data[1]['Norm (B)']), color='red', linestyle='--', label='Background')
 
-avgs = []
-for i in data:
-    avgs.append(np.mean(i['Norm (B)']/1e-6))
+    ax.bar(bar_labels, avgs, color=bar_colors[0:len(bar_labels)])
+    plt.xticks(rotation=45)
 
-print(f"\n\n{avgs[1]}\n\n")
+    # ax.plot(data[1]['TIME'], data[1]['Norm (B)'] / 1e-6, color='red',  alpha=0.5, linestyle='-', label='Background')
+    # ax.plot(data[2]['TIME'], data[2]['Norm (B)'] / 1e-6, color=bar_colors[1], alpha=0.5, linestyle='-', label='Paint A')
+    # ax.plot(data[3]['TIME'], data[3]['Norm (B)'] / 1e-6, color=bar_colors[2], linestyle='-', label='Paint B')
+    # ax.plot(data[4]['TIME'], data[4]['Norm (B)'] / 1e-6, color=bar_colors[3], alpha=0.5, linestyle='-', label='Paint C')
+    # ax.plot(data[5]['TIME'], data[5]['Norm (B)'] / 1e-6, color=bar_colors[4], alpha=0.5, linestyle='-', label='No Paint')
+    ax.set_title('Room 376B Time-Averaged Background DC Magnetic Field Norms\n')
+    ax.set_ylabel(r'Time-Averaged Norm($\vec{B}$) ($\mu$T)')   
 
-print(files)
+else:
+    ax.plot(data[0]['TIME'], data[0]['Norm (B)'] / 1e-6, color=bar_colors[0], alpha=0.5, linestyle='-', label='Breaker on')
+    ax.plot(data[1]['TIME'], data[1]['Norm (B)'] / 1e-6, color='red',  alpha=0.5, linestyle='-', label='Breaker off')
+    ax.set_title('Room 376B Background DC Magnetic Field Norms\n')
+    ax.set_ylabel(r'Norm($\vec{B}$) ($\mu$T)')    
+    ax.set_xlabel('Time (s)')
 
-# ax.bar(['Motor', 'Background', 'Paint A', 'Paint B', 'Paint C', 'No Paint'], avgs, color=bar_colors[0:5])
-# ax.axhline(y=np.mean(data[1]['Norm (B)']), color='red', linestyle='--', label='Background')
-# plt.xticks(rotation=45)
-
-ax.plot(data[0]['TIME'], data[0]['Norm (B)'] / 1e-6, color=bar_colors[0], alpha=0.5, linestyle='-', label='Motor')
-ax.plot(data[1]['TIME'], data[1]['Norm (B)'] / 1e-6, color='red',  alpha=0.5, linestyle='-', label='Background')
-ax.plot(data[2]['TIME'], data[2]['Norm (B)'] / 1e-6, color=bar_colors[1], alpha=0.5, linestyle='-', label='Paint A')
-# ax.plot(data[3]['TIME'], data[3]['Norm (B)'] / 1e-6, color=bar_colors[2], linestyle='-', label='Paint B')
-ax.plot(data[4]['TIME'], data[4]['Norm (B)'] / 1e-6, color=bar_colors[3], alpha=0.5, linestyle='-', label='Paint C')
-ax.plot(data[5]['TIME'], data[5]['Norm (B)'] / 1e-6, color=bar_colors[4], alpha=0.5, linestyle='-', label='No Paint')
 ax.legend()
 fig.set_size_inches(6, 4)
-ax.set_title('Room 376B Background DC Magnetic Field Norms\n')
-ax.set_xlabel('Time (s)')
-ax.set_ylabel(r'Norm($\vec{B}$) ($\mu$T)')    
-ax.set_ylabel(r'Time-Averaged Norm($\vec{B}$) ($\mu$T)')    
 ax.grid(True, which='major')
 
 plt.tight_layout()
